ab2294ba884b2d4f0b00ba0b698771769e2c16d2
Nico Schmidt <nicoolschmidt@live.de>
2017-09-29 22:39:47 +0200

    Spotify show info for currently playing account. Automaticaly show and hide.

--- plugins/spotify/controller.js
+++ plugins/spotify/controller.js
@@ -1,16 +1,36 @@
-function Spotify($scope, SpotifyService, SpeechService, $interval) {
+function Spotify($scope, SpotifyService, SpeechService, $interval, Focus) {
 
-    var updateSpotifyInfo = function () {
-        SpotifyService.getPlaying().then(function (item) {
-            $scope.spotify = item.item.name + " - " + item.item.artists[0].name;
-        }, function (error) {
-            $scope.spotify = {error: error};
-        });
-    };
+	var isPlaying = [];
+	var refreshKeys = config.spotify.refreshKeys;
 
-    if(typeof config.spotify != 'undefined' && config.spotify.length){
-        $interval(updateSpotifyInfo, 2000);
-    }
+	var updateSpotifyInfo = function () {
+		refreshKeys.forEach(function(key) {
+			SpotifyService.getPlaying(key).then(function (item) {
+				if(item.device.name == 'Küche' && item.device.is_active && item.is_playing) {
+					$scope.spotify = item.item.name + " - " + item.item.artists[0].name;
+					isPlaying[refreshKeys.indexOf(key)] = true;
+				}
+				else {
+					isPlaying[refreshKeys.indexOf(key)] = false;
+				}
+			}, function (error) {
+				$scope.spotify = {error: error};
+			});
+		});
+
+		if(isPlaying.indexOf(true) == -1) {
+			$scope.showSpotify = false;
+		}
+		else {
+			$scope.showSpotify = true;
+		}
+	};
+
+
+
+	if(typeof config.spotify.refreshKeys != 'undefined' && refreshKeys.length){
+		$interval(updateSpotifyInfo, 2000);
+	}
 
 }
 
--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -1,45 +1,45 @@
 (function () {
-    'use strict';
-    function SpotifyService($http, $httpParamSerializer, $q) {
-        var service = {};
+	'use strict';
+	function SpotifyService($http, $httpParamSerializer, $q) {
+		var service = {};
 
-        service.getPlaying = function () {
-            var deferred = $q.defer();
-            service.getAccessToken().then(function (response) {
-                var token = response.access_token
-                var req = {
-                    method: 'GET',
-                    url: 'https://api.spotify.com/v1/me/player',
-                    headers: {
-                        'Authorization': 'Bearer ' + token
-                    }
-                };
-                $http(req).then(function (response) {
-                    deferred.resolve(response.data);
-                });
-            });
-            return deferred.promise;
-        }
+		service.getPlaying = function (key) {
+			var deferred = $q.defer();
+			service.getAccessToken(key).then(function (response) {
+				var token = response.access_token;
+				var req = {
+					method: 'GET',
+					url: 'https://api.spotify.com/v1/me/player',
+					headers: {
+						'Authorization': 'Bearer ' + token
+					}
+				};
+				$http(req).then(function (response) {
+					deferred.resolve(response.data);
+				});
+			});
+			return deferred.promise;
+		}
 
-        service.getAccessToken = function () {
-            var deferred = $q.defer();
-            var req = {
-                method: 'POST',
-                url: 'https://accounts.spotify.com/api/token',
-                headers: {
-                    'Authorization': 'Basic ' + config.spotify.appSecret,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data: $httpParamSerializer({'grant_type': "refresh_token", 'refresh_token': config.spotify.refreshKey})
-            };
-            $http(req).then(function (response) {
-                deferred.resolve(response.data);
-            });
-            return deferred.promise;
-        }
-        return service;
-    }
+		service.getAccessToken = function (key) {
+			var deferred = $q.defer();
+			var req = {
+				method: 'POST',
+				url: 'https://accounts.spotify.com/api/token',
+				headers: {
+					'Authorization': 'Basic ' + config.spotify.appSecret,
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				data: $httpParamSerializer({'grant_type': "refresh_token", 'refresh_token': key})
+			};
+			$http(req).then(function (response) {
+				deferred.resolve(response.data);
+			});
+			return deferred.promise;
+		}
+		return service;
+	}
 
-    angular.module('SmartMirror')
+	angular.module('SmartMirror')
         .factory('SpotifyService', SpotifyService);
 }());
