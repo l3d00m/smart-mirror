367ac516357f75f4451f327936456d1bb1229b6b
Nico Schmidt <nicoolschmidt@live.de>
2017-10-14 19:06:11 +0200

    design improved

--- plugins/humidity/controller.js
+++ plugins/humidity/controller.js
@@ -8,8 +8,8 @@ function Humidity($scope, $timeout) {
 	var updateInfoIntervall = function() {
 		sensor.read(config.humidity.sensorType, config.humidity.pin, function(err, temperature, humidity) {
 			if(!err) {
-				$scope.temperatureValue = temperature.toFixed(1) + '°C';
-				$scope.humidityValue = humidity.toFixed(1) + '%';
+				$scope.temperatureValue = temperature.toFixed(1);
+				$scope.humidityValue = humidity.toFixed(1);
 			}
 		});
 		$timeout(updateInfoIntervall, 5000);
