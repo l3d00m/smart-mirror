ad6ab315cac39734eb4e78c71a3de22df157003b
Nico Schmidt <nicoolschmidt@live.de>
2018-05-30 18:12:47 +0200

    Room climate now updating from hass api

--- plugins/humidity/controller.js
+++ plugins/humidity/controller.js
@@ -1,31 +1,25 @@
 /**
  * Created by nicoo on 14.10.2017.
  */
-function Humidity($scope, $timeout) {
-
-	try{
-		var sensor = require('node-dht-sensor');
-	}
-	catch (err) {
-		console.log("node-dht-sensor module not installed")
-	}
+function Humidity($scope, $timeout, ClimateService) {
 
 	var updateInfoIntervall = function() {
-		sensor.read(config.humidity.sensorType, config.humidity.pin, function(err, temperature, humidity) {
-			if(!err) {
-				$scope.temperatureValue = temperature.toFixed(1);
-				$scope.humidityValue = humidity.toFixed(1);
-			}
-		});
+
+		ClimateService.getTemperature()
+			.then(data => {
+				$scope.temperatureValue = data.state;
+			});
+
+		ClimateService.getHumidity()
+			.then(data => {
+				$scope.humidityValue = data.state;
+			});
+
 		$timeout(updateInfoIntervall, 5000);
 	}
 
-	if (typeof config.humidity !== 'undefined' && typeof config.humidity.pin === 'number' && typeof sensor != "undefined") {
+	if (typeof config.climate !== 'undefined') {
 		updateInfoIntervall();
-	}
-	else {
-		$scope.temperatureValue = 21.1;
-		$scope.humidityValue = 64.8;
 	}
 
 }
